79699e322c3e230a54039200c76bb7fe2744c99c
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-05-27 20:06:50 +0530

    dymmy User can select subscription

--- src/Components/Cart Page/AddSubscription/AddSubscription.jsx
+++ src/Components/Cart Page/AddSubscription/AddSubscription.jsx
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddSubscription.css";
 
 function AddSubscription(props) {
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+  const [subsPrice, setSubsPrice] = useState(0);
+
+  // Calculate Subscription
+  const onClickCalculate = () => {
+    setSubsPrice(250);
+  };
+
   return (
     <div className=" AddSubscription-div ">
       <div className="AddSubscription-div__left">
         <h3>Add As Subscription</h3>
         <div className="AddSubscription-div__left__time">
           <p>
-            From: <span>20/25/25</span>
+            From: <span>{start}</span>
           </p>
           <p>
-            To: <span>20/25/25</span>
+            To: <span>{end}</span>
           </p>
         </div>
         <p>
-          Daily: <span>255</span>
+          Daily: <span>{subsPrice}</span>
         </p>
       </div>
       <div className="AddSubscription-div__right">
         <div className="inputDiv">
-          <input type="date" name="" id="startingDate" />
-          <input type="date" name="" id="endingDate" />
+          <input
+            onChange={(e) => {
+              setStart(e.target.value);
+            }}
+            type="date"
+            name=""
+            id="startingDate"
+          />
+          <input
+            onChange={(e) => {
+              setEnd(e.target.value);
+            }}
+            type="date"
+            name=""
+            id="endingDate"
+          />
         </div>
         <div className="btnDiv">
-          <button>CALCULATE</button>
+          <button onClick={onClickCalculate}>CALCULATE</button>
           <button>CANCLE</button>
         </div>
       </div>
