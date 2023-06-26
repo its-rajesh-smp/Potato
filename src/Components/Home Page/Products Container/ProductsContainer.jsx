aee4cd18db7ff7fba106a0f57f49d70ece83820b
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-06-26 21:15:01 +0530

    Added Filter

--- src/Components/Home Page/Products Container/ProductsContainer.jsx
+++ src/Components/Home Page/Products Container/ProductsContainer.jsx
@@ -1,13 +1,13 @@
 import React, { useCallback } from "react";
 import "./ProductsContainer.css";
 import Product from "../../UI/Home Page/Product/Product";
-import { shallowEqual, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 function ProductContainer(props) {
   const products = useSelector((state) => state.productSlice.products);
   const cartItems = useSelector((state) => state.userCartSlice.cart);
 
-
+  console.log(props.filterParam.trim());
 
 
 
@@ -20,7 +20,7 @@ function ProductContainer(props) {
         return <Product quantity={quantity} key={product.id} data={product} />;
       }).filter((product) => {
         const productData = product.props.data
-        if (productData.name.toLowerCase().trim().includes(props.searchParam.toLowerCase().trim()) || productData.price == props.searchParam || props.searchParam.trim() === "") {
+        if ((productData.name.toLowerCase().trim().includes(props.searchParam.toLowerCase().trim()) || productData.price == props.searchParam || props.searchParam.trim() === "") && (props.filterParam === productData.type || props.filterParam == "")) {
           return true
         }
       })}
--- src/Components/UI/Home Page/VegNonVegFilter/VegNonVegFilter.jsx
+++ src/Components/UI/Home Page/VegNonVegFilter/VegNonVegFilter.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./VegNonVegFilter.css"
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
+import { FormGroup, RadioGroup } from '@mui/material';
 
 
 
 
 function VegNonVegFilter(props) {
+    const [veg, setVeg] = useState(false)
+    const [nonVeg, setNonVeg] = useState(false)
+
+
+    const onClickHandeler = (e) => {
+        if (e.target.value === "VEG") {
+            if (!e.target.checked) { setVeg(false); props.setFilterParam("") }
+            else {
+                setVeg(true)
+                props.setFilterParam("veg")
+            }
+            setNonVeg(false)
+        }
+        else {
+            if (!e.target.checked) { setNonVeg(false); props.setFilterParam("") }
+            else {
+                setNonVeg(true)
+                props.setFilterParam("non-veg")
+            }
+            setVeg(false);
+        }
+    }
+
     return (
         <div className="flipswitch">
-            <FormControlLabel control={<Switch size='small' />} label="Veg" />
-            <FormControlLabel control={<Switch size='small' />} label="Non-Veg" />
+
+            <FormControlLabel control={<Switch onClick={onClickHandeler} checked={veg} value={"VEG"} name='filter' size='small' />} label="Veg" />
+            <FormControlLabel control={<Switch onClick={onClickHandeler} checked={nonVeg} name='filter' size='small' />} label="Non-Veg" value={"NONVEG"} />
+
+
         </div>
 
     );
