8b74d05359c3930a0802da67f0cf570bac72b2bb
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-06-26 12:04:27 +0530

    user can go to my orders page from profile

--- src/Components/UI/User Profile Page/User/User.jsx
+++ src/Components/UI/User Profile Page/User/User.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./User.css";
+import { useNavigate } from "react-router-dom";
 
 function User(props) {
+  const navigate = useNavigate()
+  const onClickMyOrders = () => {
+    navigate("/myorder")
+  }
+
   return (
     <div className=" User-div ">
       <i className="bx bxs-user"></i>
       <p>Rajesh Singha Maha Patra</p>
+      <p onClick={onClickMyOrders}>My Orders</p>
     </div>
   );
 }
